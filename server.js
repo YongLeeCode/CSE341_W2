566f06cb22c30e7e08b6a5bdda24006f98dd53d6
Yong <yonghwan311@gmail.com>
2023-05-05 14:26:56 -0600

    reset

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(`/contacts`, require(`./routes`));
+app.use(`/`, require(`./routes`));
 
 mongodb.initDb((err) => {
   if (err) {
