fde0267a2dd51496fe05d1e0bd12d6eedb015214
Yong <yonghwan311@gmail.com>
2023-05-05 14:23:16 -0600

    wk3

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(`/`, require(`./routes`));
+app.use(`/contacts`, require(`./routes`));
 
 mongodb.initDb((err) => {
   if (err) {
